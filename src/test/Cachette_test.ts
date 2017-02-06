1a9f7e7bcf2918f1bcd80f3a260f9b99cbf31436
Eryk Warren <erykwarren@users.noreply.github.com>
2017-02-06 10:49:20 -0500

    Handle errors while fetching. Add tests. Do not await in parallel (#1)

--- src/test/Cachette_test.ts
+++ src/test/Cachette_test.ts
@@ -7,7 +7,7 @@ import * as sinon from 'sinon';
 import * as redis from 'redis';
 import { EventEmitter } from 'events';
 
-import { Cachette } from '../lib/Cachette';
+import { Cachette, fetchingFunction } from '../lib/Cachette';
 import { LocalCache } from '../lib/LocalCache';
 import { RedisCache } from '../lib/RedisCache';
 
@@ -97,6 +97,177 @@ describe('Cachette', () => {
 
   });
 
+  describe('getOrFetchValue', () => {
+    beforeEach(Cachette.connect);
+    afterEach(Cachette.disconnect);
+
+
+    it('does not fetch if value in cache', async () => {
+      let numCalled = 0;
+      const object = {
+        fetch: async (value) => {
+          numCalled++;
+          return value;
+        },
+      };
+
+      const cache = Cachette.getCacheInstance();
+      cache.setValue('key', 'value');
+      const value = await Cachette.getOrFetchValue(
+        'key',
+        10,
+        false,
+        <fetchingFunction> object.fetch,
+        object,
+        'newvalue',
+      );
+      expect(value).to.eql('value');
+      expect(numCalled).to.eql(0);
+
+    });
+
+    it('fetches if value not in cache', async () => {
+      let numCalled = 0;
+      const object = {
+        fetch: async (value) => {
+          numCalled++;
+          return value;
+        },
+      };
+
+      const cache = Cachette.getCacheInstance();
+      cache.setValue('key2', 'value');
+      const value = await Cachette.getOrFetchValue(
+        'key',
+        10,
+        false,
+        <fetchingFunction> object.fetch,
+        object,
+        'newvalue',
+      );
+      expect(value).to.eql('newvalue');
+      expect(numCalled).to.eql(1);
+
+    });
+
+    it('fetches once if multiple simultaneous requests', async () => {
+      let numCalled = 0;
+      const object = {
+        fetch: async (value) => {
+          numCalled++;
+          return value;
+        },
+      };
+
+      const cache = Cachette.getCacheInstance();
+      cache.setValue('key2', 'value');
+
+      const callGetOrFetch = () => Cachette.getOrFetchValue(
+        'key',
+        10,
+        false,
+        <fetchingFunction> object.fetch,
+        object,
+        'newvalue',
+      );
+
+      const calls = [];
+      for (let i = 0; i < 100; i++) {
+        calls.push(callGetOrFetch());
+      }
+
+      const values = await Promise.all(calls);
+      expect(values.length).to.eql(100);
+      for (const value of values) {
+        expect(value).to.eql('newvalue');
+      }
+      expect(numCalled).to.eql(1);
+
+    });
+
+    it('fetches once each if multiple simultaneous of two requests', async () => {
+      let numCalled1 = 0;
+      let numCalled2 = 0;
+      const object = {
+        fetch1: async (value) => {
+          numCalled1++;
+          return value;
+        },
+        fetch2: async (value) => {
+          numCalled2++;
+          return `${value}bis`;
+        },
+      };
+
+      const callGetOrFetch = (key, fn) => Cachette.getOrFetchValue(
+        key,
+        10,
+        false,
+        fn,
+        object,
+        'newvalue',
+      );
+
+      const calls = [];
+
+      for (let i = 0; i < 100; i++) {
+        const fn = (i % 2) ? object.fetch1 : object.fetch2;
+        const key = (i % 2) ? 'key1' : 'key2';
+        calls.push(callGetOrFetch(key, fn as fetchingFunction));
+      }
+
+      const values = await Promise.all(calls);
+      expect(values.length).to.eql(100);
+      let count1 = 0;
+      let count2 = 0;
+      for (const value of values) {
+        if (value === 'newvalue') {
+          count1++;
+        } else if (value === 'newvaluebis') {
+          count2++;
+        } else {
+          expect(value).to.eql('newvalue');
+        }
+      }
+      expect(numCalled1).to.eql(1);
+      expect(numCalled2).to.eql(1);
+      expect(count1).to.eql(50);
+      expect(count2).to.eql(50);
+
+    });
+
+    it('handles errors during simultaneous requests', async () => {
+      const object = {
+        fetch: async (): Promise<number> => {
+          throw new Error('basta');
+        },
+      };
+
+      const callGetOrFetch = () => Cachette.getOrFetchValue(
+        'key',
+        10,
+        false,
+          <fetchingFunction> object.fetch,
+        object,
+        'newvalue',
+      );
+
+      const calls = [];
+      for (let i = 0; i < 10; i++) {
+        calls.push(callGetOrFetch());
+      }
+
+      let numExceptions = 0;
+      for (const call of calls) {
+        await call.catch(() => numExceptions++);
+      }
+
+      expect(numExceptions).to.eql(10);
+
+    });
+
+  });
+
   describe('decorator cached()', () => {
     interface Response {
       variant: string;
@@ -143,4 +314,3 @@ describe('Cachette', () => {
   });
 
 });
-
