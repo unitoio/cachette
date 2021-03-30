6a22dfc5912d9e98294e7adfd39a43ea6e27ba60
Ronan Jouchet <ronanj@unito.io>
2021-03-30 11:44:30 -0400

    🔧 Attempt to fix build & publish
    Bump & push tag is already done in `bump_node_package_version.sh` ,
    we shouldn't re-do it in `publish_public_npm.sh`

--- scripts/publish_public_npm.sh
+++ scripts/publish_public_npm.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 # Exit if PULL_REQUEST is true or else publish
-
 if [[ -n "$PULL_REQUEST" && "$PULL_REQUEST" != "false" ]]; then
   echo "Not publishing the package from a Pull Request."
   exit 0
 fi
 
-npm run publish-to-npm
+npm publish
