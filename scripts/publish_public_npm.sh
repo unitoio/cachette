6337b8c41a386b6bb6086a0a79ed2da88f2a144f
louib <code@louib.net>
2021-04-28 13:33:34 -0400

    🔧 Migrating CI to GitHub actions (#52)
    * Remove code coverage report from test:ci
    
    * Add first GH workflows
    
    * Purging shippable
    
    * No need for a token for only public deps
    
    * No need to cleanup between jobs
    
    * Add redis url to tests.
    
    * Setup redis in test workflow
    
    * Adding lint job
    
    * Add script to publish on merge
    
    * Configure npm
    
    * Remove another GITHUB_PKG from env.
    
    * Enable publishing to NPM
    
    * Remove commented out descriptions

--- scripts/publish_public_npm.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Exit if PULL_REQUEST is true or else publish
-if [[ -n "$PULL_REQUEST" && "$PULL_REQUEST" != "false" ]]; then
-  echo "Not publishing the package from a Pull Request."
-  exit 0
-fi
-
-npm publish
