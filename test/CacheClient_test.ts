930dfb3afe2f70c8dacc1d0f79f7a6e680dcbe23
Jehan <jehanr@unito.io>
2021-07-19 11:35:40 -0400

    add test

--- test/CacheClient_test.ts
+++ test/CacheClient_test.ts
@@ -215,6 +215,12 @@ describe('CacheClient', () => {
 
     });
 
+    it('will convert plain object values', async () => {
+      const cacheClient = new MyCacheClient();
+      const key = cacheClient['buildCacheKey']('functionName', ['argument', { argument1: 'arg1', argument2: 'arg2' }, new Date()]);
+      expect(key).to.equal('functionName-argument-arg1-arg2');
+    })
+
   });
 
 });
