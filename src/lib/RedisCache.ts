18b9335495a234382aa6302fc9ec1473215111f5
Ian Fillion-de Kiewit <ianfdk@unito.io>
2022-11-25 13:47:52 -0500

    fix test

--- src/lib/RedisCache.ts
+++ src/lib/RedisCache.ts
@@ -58,15 +58,13 @@ export class RedisCache extends CacheInstance {
       enableOfflineQueue: false,
     });
     this.redisClient.scan
-    // Hack until Redlock 5.x is out of beta
-    const redlockCompatibleClient = this.redisClient as unknown as Redlock.CompatibleRedisClient;
-    this.redlock = new Redlock([redlockCompatibleClient], {
+    this.redlock = new Redlock([this.redisClient as unknown as Redlock.CompatibleRedisClient], { // Hack until Redlock 5.x is out of beta
       driftFactor: RedisCache.REDLOCK_CLOCK_DRIFT_FACTOR,
       retryCount: RedisCache.REDLOCK_RETRY_COUNT,
       retryDelay: RedisCache.REDLOCK_RETRY_DELAY_MS,
       retryJitter: RedisCache.REDLOCK_JITTER_MS,
     });
-    this.redlockWithoutRetry = new Redlock([redlockCompatibleClient], {
+    this.redlockWithoutRetry = new Redlock([this.redisClient as unknown as Redlock.CompatibleRedisClient], { // Hack until Redlock 5.x is out of beta
       driftFactor: RedisCache.REDLOCK_CLOCK_DRIFT_FACTOR,
       retryCount: 0,
       retryDelay: 0,
